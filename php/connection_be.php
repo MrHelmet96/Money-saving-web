050f6a9ae1b8ee372777badefde3fd3e00db7fe3
MrHelmet96 <alejandromatiascasco@gmail.com>
2022-11-21 11:34:28 -0300

    Uploading connection file

---
+++ php/connection_be.php
@@ -0,0 +1,12 @@
+<?php
+
+    $connection = mysqli_connect("localhost", "root", "", "login_register_db");
+
+    
+    // if($connection){
+    //     echo 'Conectado exitosamente a la Base de Datos';
+    // }else{
+    //     echo 'No se ha podido conectar a la Base de Datos';
+    // }
+
+?>
